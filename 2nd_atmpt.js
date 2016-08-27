ae8583f285e523e7b79515e707b2a314ceb38747
njericooper <njericooper@gmail.com>
2016-08-27 11:56:58 -0500

    2nd Attempt
    This is JSON to like the key to my Git(;

---
+++ 2nd_atmpt.js
@@ -0,0 +1,6 @@
+{
+    "token": 95226313ff3c40ae49f6242aa44824f,
+    //"id": "06AKEBrKUckW0KREUWRnvT",
+    //"uri": "spotify:track:06AKEBrKUckW0KREUWRnvT",
+    "github": "https://github.com/njericooper/code2040",
+  }
