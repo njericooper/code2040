af9e1054594b95136acc5cf42b726798593ae161
njericooper <njericooper@gmail.com>
2017-01-08 13:32:13 -0600

    update

--- code912.js
+++ code912.js
@@ -7,9 +7,9 @@ $.ajax({
    success: function(data) {
 
       var send = {
-        "token" : "http://challenge.code2040.org/api/register"+"hey",
+        "token" : "h295226313ff3c40ae49f6242aa44824f",
         "github" : "https://github.com/njericooper"
 
    },
-   type: 'POST'
+   type: 'POST',        
 });
